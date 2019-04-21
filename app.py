19f0899961255f19c861cb7c82ded4fd612fe9a6
topgun93 <38679840+topgun93@users.noreply.github.com>
2019-04-22 00:23:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def settings():
 # ======== Main ============================================================== #
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)  # Generic key for dev purposes only
-    app.run(debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', debug=True, use_reloader=True)
